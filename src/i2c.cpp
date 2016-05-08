e36d492f2a43d62b557a68fd3a4ae098706aa2d9
Matthias Schmid <ramsondon@gmail.com>
2016-05-08 12:33:50 +0200

    added doc, changed varname

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -6,7 +6,7 @@ bool I2C::sWireInit = false;
 
 I2C::I2C(uint8_t slave_address)
 {
-    mSlave = slave_address;
+    mSlaveAddress = slave_address;
     I2C::init();
 }
 
@@ -20,31 +20,31 @@ void I2C::init(void)
 
 uint8_t I2C::read(uint8_t address)
 {
-    uint8_t data;                           // 'data' will store the register data   
-    Wire.beginTransmission(mSlave);         // Initialize the Tx buffer
-    Wire.write(address);                    // Put slave register address in Tx buffer
-    Wire.endTransmission(false);            // Send the Tx buffer, but send a restart to keep connection alive
-    Wire.requestFrom(mSlave, (uint8_t) 1);  // Read one byte from slave register address 
-    data = Wire.read();                     // Fill Rx buffer with result
-    return data;                            // Return exactly one byte
+    uint8_t data;                                   // 'data' will store the register data   
+    Wire.beginTransmission(mSlaveAddress);          // Initialize the Tx buffer
+    Wire.write(address);                            // Put slave register address in Tx buffer
+    Wire.endTransmission(false);                    // Send the Tx buffer, but send a restart to keep connection alive
+    Wire.requestFrom(mSlaveAddress, (uint8_t) 1);   // Read one byte from slave register address 
+    data = Wire.read();                             // Fill Rx buffer with result
+    return data;                                    // Return exactly one byte
 }
 
-void I2C::read(uint8_t address, uint8_t count, uint8_t * dest) 
+void I2C::read(uint8_t address, uint8_t count, uint8_t * buf) 
 {
-    Wire.beginTransmission(mSlave);     // Initialize the Tx buffer
-    Wire.write(address);                // Put slave register address in Tx buffer
-    Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
+    Wire.beginTransmission(mSlaveAddress);  // Initialize the Tx buffer
+    Wire.write(address);                    // Put slave register address in Tx buffer
+    Wire.endTransmission(false);            // Send the Tx buffer, but send a restart to keep connection alive
     uint8_t i = 0;
-    Wire.requestFrom(mSlave, count);    // Read bytes from slave register address 
+    Wire.requestFrom(mSlaveAddress, count); // Read bytes from slave register address 
     while (Wire.available()) {
-        dest[i++] = Wire.read();        // Put read results in the Rx buffer
+        buf[i++] = Wire.read();            // Put read results in the Rx buffer
     }
 }
 
 void I2C::write(uint8_t address, uint8_t data)
 {
-    Wire.beginTransmission(mSlave); // Initialize the Tx buffer
-    Wire.write(address);            // Put slave register address in Tx buffer
-    Wire.write(data);               // Put data in Tx buffer
-    Wire.endTransmission();         // Send the Tx buffer
+    Wire.beginTransmission(mSlaveAddress);  // Initialize the Tx buffer
+    Wire.write(address);                    // Put slave register address in Tx buffer
+    Wire.write(data);                       // Put data in Tx buffer
+    Wire.endTransmission();                 // Send the Tx buffer
 }
--- src/i2c.h
+++ src/i2c.h
@@ -6,19 +6,50 @@
 
 #include <stdint.h>
 
-
+/*
+ * class I2C
+ * 
+ * communication middleware for i2c with arduino.
+ */
 class I2C
 {
     private:
-        uint8_t mSlave;
+        /*
+         * slave address on the arduino board
+         */
+        uint8_t mSlaveAddress;
+        
+        /*
+         * static Wire.begin() for using multiple i2c objects
+         */
         static bool sWireInit;
+        
+        /*
+         * initialization of the arduino Wire api
+         */
         static void init(void);
         
     public:
-      I2C(uint8_t slave_address);
-      uint8_t read(uint8_t address);
-      void read(uint8_t address, uint8_t count, uint8_t * dest);
-      void write(uint8_t address, uint8_t data);
+        
+        /*
+         * constructor 
+         */
+        I2C(uint8_t slave_address);
+        
+        /*
+         * reads a single byte at address 'address' of device mSlave
+         */
+        uint8_t read(uint8_t address);
+        
+        /*
+         * reads #'count' bytes at address 'address' of device mSlave
+         */
+        void read(uint8_t address, uint8_t count, uint8_t * buf);
+        
+        /*
+         * write data to address of mSlave device
+         */
+        void write(uint8_t address, uint8_t data);
 };
 
 #endif
